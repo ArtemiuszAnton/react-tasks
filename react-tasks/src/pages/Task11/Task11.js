c6409338045962ac1b866c509d421a2dad0ba388
ArtemiuszAnton <elvispresley9122@gmail.com>
2024-04-17 23:13:12 +0200

    add finish version tasks 11-14

--- react-tasks/src/pages/Task11/Task11.js
+++ react-tasks/src/pages/Task11/Task11.js
@@ -1,8 +1,22 @@
+import draniki from '../Task11/assets/draniki.jpg';
+import pelmeni from './assets/pelmeni.webp';
+import kebab from './assets/kebab.png';
+
 function Task11() {
 
+    const arr = [{ title: 'draniki', description: 'potato grated', image: draniki },
+    { title: 'pelmeni', description: 'Believers in the dough welded', image: pelmeni },
+    { title: 'kebab', description: 'meat with vegetables in Lavash', image: kebab }].map(el => {
+        return <div>
+            <h1>{el.title}</h1>
+            <h5>{el.description}</h5>
+            <img src={el.image} />
+        </div>
+    });
 
 
-    return <div>
+
+    return <>
         <p>
             5. Реализуйте компонент, который принимает массив объектов с полями title –
             название рецепта, description – описание рецепта и image – картинка блюда. Для
@@ -10,8 +24,9 @@ function Task11() {
             Добавьте немного стилей
 
         </p>
+
         {arr}
-    </div>
+    </>
 }
 
 

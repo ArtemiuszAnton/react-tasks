341e7e409a2e5d8f48384e39d5033482d93b0cb9
ArtemiuszAnton <elvispresley9122@gmail.com>
2024-05-18 23:31:22 +0200

    add tasks

---
+++ react-tasks/src/pages/Task80/Task80.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+
+function Task80() {
+
+
+
+    return <>
+        <p>
+            5. Создайте компонент, который позволяет пользователю добавлять и удалять
+            элементы из списка. Используйте useCallback, чтобы оптимизировать функции
+            добавления и удаления элементов
+        </p>
+
+
+        <button style={{ background: 'pink', display: 'block', marginTop: 30, borderRadius: 4 }}><Link to={'/'}>Вернуться на главную</Link></button >
+
+    </>
+}
+
+
+export default Task80
